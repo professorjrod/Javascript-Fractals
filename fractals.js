712243ab56c61e5a37dc04516c005eda00883209
Jared <mumawjared@gmail.com>
2022-10-10 15:41:55 -0700

    setup canvas

--- fractals.js
+++ fractals.js
@@ -1,6 +1,16 @@
-const canvas = document.getElementById('canvas');
-
-const clearRender = () => {
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function setup() {
+    createCanvas(1000, 1000);
+    pixelDensity(1);
+    loadPixels();
+    for (let x = 0; x < width; x++) {
+        for(let y = 0; y < height; y++) {
+            let fx = (x + y * width) * 4;
+            pixels[fx + 0] = 255;
+            pixels[fx + 1] = 51;
+            pixels[fx + 2] = 51;
+            pixels[fx + 3] = 255;
+        }
+    }
+    updatePixels();
 }
+
